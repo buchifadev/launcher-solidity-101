c294939c32fb0914091265235baee200d18e8ddf
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-07 15:41:13 +0400

    fixes and cleanup

--- Launcher.sol
+++ Launcher.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.0;
 contract Launcher {
     uint256 constant COST_PER_VIEW = 1 ether; // cost per advert view
     uint256 constant MINIMUM_DEPOSIT = 5 ether; // mimimum deposit advertiser must pay for advert creation
-    uint256 counter = 0;
-    uint256[] activeAdverts;
+    uint256 advertsCounter = 0;
+    mapping(uint256 => bool) activeAdverts;
     mapping(uint256 => Advert) allAdverts;
     mapping(uint256 => mapping(address => bool)) alreadyViewed;
     address payable platformAccount;
@@ -33,17 +33,20 @@ contract Launcher {
         platformAccount = payable(msg.sender);
     }
 
-    // create a new advert and send to platform
+    /// @dev create a new advert and send to platform
     function createAdvert(
-        string memory name,
-        string memory description,
-        string memory imageUrl
-    ) public payable {
+        string calldata name,
+        string calldata description,
+        string calldata imageUrl
+    ) external payable {
+        require(bytes(name).length > 0, "Empty name");
+        require(bytes(description).length > 0, "Empty description");
+        require(bytes(imageUrl).length > 0, "Empty image Url");
         require(
             msg.value >= MINIMUM_DEPOSIT,
             "Amount sent must be greater or equal to minimum deposit"
         );
-        allAdverts[counter] = Advert(
+        allAdverts[advertsCounter] = Advert(
             msg.sender,
             name,
             description,
@@ -51,69 +54,56 @@ contract Launcher {
             msg.value,
             0
         );
-        activeAdverts.push(counter);
-        counter += 1;
+        activeAdverts[advertsCounter] = true;
+        advertsCounter += 1;
     }
 
-    // simulates the viewing of an advertisement
-    // In real life, this could be some videos, images, or GIFs
-    function viewAdvert(uint256 advertIndex) public {
+    /**
+     * @dev simulates the viewing of an advertisement
+     * In real life, this could be some videos, images, or GIFs
+     */
+    function viewAdvert(uint256 advertIndex) external payable {
+        // proceed with transaction only when the advert is still active
+        require(activeAdverts[advertIndex], "Advert is not active");
+        Advert storage currentAdvert = allAdverts[advertIndex];
         require(
-            allAdverts[advertIndex].owner != msg.sender,
+            currentAdvert.owner != msg.sender,
             "You can't view your own advert"
         );
         require(
             !alreadyViewed[advertIndex][msg.sender],
             "You have already viewed this advert"
         );
-        bool isActive = false;
-        // first check if advert is still active (advert still has enough funds to pay both platform and viewer)
-        for (uint256 i = 0; i < activeAdverts.length; i++) {
-            if (activeAdverts[i] == advertIndex) {
-                isActive = true;
-                break;
-            }
-        }
-        // proceed with transaction only when the advert is still active
-        require(isActive, "Advert is not active");
+
         // deduct the cost of this view from the advert balance first
-        allAdverts[advertIndex].balance -= COST_PER_VIEW;
+        uint balanceLeft = currentAdvert.balance - COST_PER_VIEW;
+        currentAdvert.balance = balanceLeft;
         // increase advert view count
-        allAdverts[advertIndex].views += 1;
+        currentAdvert.views += 1;
         // keep track of views
         accumulatedViews += 1;
-        // pay the viewer for viewing the advert
-        uint256 value = COST_PER_VIEW / 2;
-        payable(msg.sender).transfer(value);
         // prevent users from viewing advert more than once
         alreadyViewed[advertIndex][msg.sender] = true;
 
         // check if advert still has enough funds for the next round
-        uint256 adBal = allAdverts[advertIndex].balance;
         // deactivate advert if it does not have enough funds to pay both viewer and platform
-        if (adBal < COST_PER_VIEW) {
-            // first get advert index from array of active adverts
-            int256 index = -1;
-            for (uint256 i = 0; i < activeAdverts.length; i++) {
-                if (i == advertIndex) {
-                    index = int256(i);
-                    break;
-                }
-            }
-            // remove index from array if it exists
-            if (index >= 0) {
-                activeAdverts[uint256(index)] = activeAdverts[
-                    activeAdverts.length - 1
-                ];
-                activeAdverts.pop();
-            }
+        if (currentAdvert.balance < COST_PER_VIEW) {
+            activeAdverts[advertIndex] = false;
         }
+        // pay the viewer for viewing the advert
+        uint256 value = COST_PER_VIEW / 2;
+        (bool success, ) = payable(msg.sender).call{value: value}("");
+        require(success, "Payment of advert failed");
     }
 
-    // Add funds to an advert balance
+    /**
+     * @dev Add funds to an advert balance
+     * @notice value sent with transaction will be the amount credited to balance of advert
+     *  */
     function creditAdvert(uint256 advertIndex) public payable {
+        Advert storage currentAdvert = allAdverts[advertIndex];
         require(
-            allAdverts[advertIndex].owner == msg.sender,
+            currentAdvert.owner == msg.sender,
             "Only advert owner can add funds to advert"
         );
         require(
@@ -121,29 +111,25 @@ contract Launcher {
             "Amount sent not enough. Minimum is 1 ether"
         );
         // update advert balance
-        allAdverts[advertIndex].balance += msg.value;
-        bool isActive = false;
-        // active advert after topping up
-        for (uint256 i = 0; i < activeAdverts.length; i++) {
-            if (activeAdverts[i] == advertIndex) {
-                isActive = true;
-                break;
-            }
-        }
-        // add advert to array to active adverts
-        if (!isActive) {
-            activeAdverts.push(advertIndex);
+        uint newBalance = currentAdvert.balance + msg.value;
+        currentAdvert.balance = newBalance;
+        // if advert wasn't active, it is set back to active
+        if (!activeAdverts[advertIndex]) {
+            activeAdverts[advertIndex] = true;
         }
     }
 
-    // Users check list of all available adverts they can view
-    // Only the indexes of the adverts are displayed
-    function allActiveAdverts() public view returns (uint256[] memory) {
-        return activeAdverts;
+    /**
+     * @dev Users check list of all available adverts they can view
+     * @return status of advert with index of advertIndex
+     *  */
+    function allActiveAdverts(uint256 advertIndex) public view returns (bool) {
+        return activeAdverts[advertIndex];
     }
 
-    // Display advert details
-    // Only displayed to the owner of advert and platform account
+    /**
+     * @return advert details
+     * */
     function advertDetails(uint256 advertIndex)
         public
         view
@@ -156,11 +142,6 @@ contract Launcher {
             uint256
         )
     {
-        require(
-            (msg.sender == allAdverts[advertIndex].owner) ||
-                (msg.sender == platformAccount),
-            "Unauthorized to call this function"
-        );
         return (
             allAdverts[advertIndex].owner,
             allAdverts[advertIndex].name,
@@ -171,35 +152,28 @@ contract Launcher {
         );
     }
 
-    // claim the platform fees stored in contract
-    function claimFees() public isPlatformAccount {
-        uint256 accumulatedFees = (accumulatedViews * 1 ether) / 2;
-        payable(platformAccount).transfer(accumulatedFees);
+    /// @dev claim the platform fees stored in contract
+    function claimFees() public payable isPlatformAccount {
+        uint256 accumulatedFees = accumulatedViews * 0.5 ether;
         // reset accumulated views
         accumulatedViews = 0;
+        (bool success, ) = payable(platformAccount).call{
+            value: accumulatedFees
+        }("");
+        require(success, "Failed to claim due fees");
     }
 
-    // check accumulated fees stored in contract
-    function viewAccumulatedFees()
+    /// @return accumulated fees and views in contract
+    function viewAccumulationDetails()
         public
         view
         isPlatformAccount
-        returns (uint256)
+        returns (uint256, uint256)
     {
-        return address(this).balance;
+        return (accumulatedViews, accumulatedViews * 0.5 ether);
     }
 
-    // check total views accumulated
-    function viewAccumulatedViews()
-        public
-        view
-        isPlatformAccount
-        returns (uint256)
-    {
-        return accumulatedViews;
-    }
-
-    // check the minimum deposit on an advert
+    /// @dev check the minimum deposit on an advert
     function viewMinimumDeposit() public pure returns (uint256) {
         return MINIMUM_DEPOSIT;
     }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - A contract where other organizations can come to advertise their products. Some of the features includes: 
     - Anybody can create an advert
     - Other users can manually view a product being advertised and get incentivised for it    
-    - Advertisers are billed according to how the number of views their advertisement get
+    - Advertisers are billed according to the number of views their advertisement get
     - Advertisers pay a minimum down payment on creating an advert
     - For every view on the advert, a certain percentage is deducted from the down payment, 
 
